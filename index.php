bd718b71a9fbfb070b4e464bb58e2c08e8c9b6d1
Sathish SP <104225470+Satya0421@users.noreply.github.com>
2023-09-16 21:29:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
-<?php include_once("html/index.html"); ?>
-<?php include_once("html/about.html"); ?>
+<?php include_once("html/index.html");
+$routes = [
+    '/' => 'home.html',
+    '/about' => 'about.html',
+    '/contact' => 'contact.html',
+];
+?>
