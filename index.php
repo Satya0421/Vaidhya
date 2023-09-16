1e4235e954a842494d54941a2016d7bafd5df705
Sathish SP <104225470+Satya0421@users.noreply.github.com>
2023-09-16 22:02:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,27 @@
-<?php include_once("html/index.html");
-$routes = [
-    '/' => 'home.html',
-    '/about' => 'about.html',
-    '/contact' => 'contact.html',
-];
-?>
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <?php include_once("html/header.html"); ?>
+    
+    <?php
+        // Get the requested page from the URL (e.g., /about, /contact)
+        $requested_page = $_SERVER['REQUEST_URI'];
+        
+        // Map the requested URL to the corresponding HTML file
+        $page_mapping = [
+            '/about' => 'html/about.html',
+            '/contact' => 'html/contact.html',
+        ];
+        
+        // Check if the requested page exists in the mapping
+        if (array_key_exists($requested_page, $page_mapping)) {
+            include_once($page_mapping[$requested_page]);
+        } else {
+            // Handle 404 - Page not found
+            include_once("html/404.html");
+        }
+    ?>
+
+    <?php include_once("html/footer.html"); ?>
+</body>
+</html>
